157dd3f21759ffd190ea97c665e80db47f6212a1
JohnK <john@jkemp.net>
2013-04-25 17:04:50 -0400

    adding first example code

---
+++ examples/first_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <websocket.h>
+
+static void on_open( web_socket *ws ){
+  printf("on open called!\n") ;
+}
+
+int main(int argc, char *argv[ ])
+{
+  web_socket *ws = websocket_new() ;
+  websocket_init( ws, "ws://localhost:8080" ) ;
+  ws->on_open = &on_open ;
+  return 0 ;
+}
